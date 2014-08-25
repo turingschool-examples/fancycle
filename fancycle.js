c8654a2db54ed94eeeaff4f639ccd04e8bb80c59
novohispano <novohispano@gmail.com>
2014-08-24 22:14:32 -0600

    Add first example

--- fancycle.js
+++ fancycle.js
@@ -1,20 +1,5 @@
 $(document).ready(function(){
-  toggleSong("#justin", "#baby");
-  toggleSong("#selena", "#come-and-get-it");
-  toggleSong("#miley", "#wrecking-ball");
-});
-
-function toggleSong (artist, song) {
-  $(artist).click(function(){
-    var buttonText  = $(this).text(),
-        textLength  = buttonText.length - 1,
-        text        = buttonText.substr(0, textLength),
-        counterText = buttonText.charAt(textLength),
-        count       = parseInt(counterText);
-
-    if ($(song).attr("class") === "song hidden") { count++ }
-
-    $(this).text(text + count);
-    $(song).toggleClass("hidden");
+  $("#justin").click(function(){
+    $("#baby").toggleClass("hidden");
   });
-}
+});
