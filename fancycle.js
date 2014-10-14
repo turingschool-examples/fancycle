a98fd62fd6e1568228faa7cae07f9c3322a272f2
novohispano <novohispano@gmail.com>
2014-10-13 22:31:53 -0600

    Add total functionality

--- fancycle.js
+++ fancycle.js
@@ -3,6 +3,18 @@ $(document).ready(function(){
   toggleSong("#selena", "#come-and-get-it")
   toggleSong("#miley",  "#wrecking-ball")
 
+  colorizeSong("#baby")
+  colorizeSong("#come-and-get-it")
+  colorizeSong("#wrecking-ball")
+
+  increaseText("#baby")
+  increaseText("#come-and-get-it")
+  increaseText("#wrecking-ball")
+
+  decreaseText("#baby")
+  decreaseText("#come-and-get-it")
+  decreaseText("#wrecking-ball")
+
   function toggleSong(artistID, songID){
     $(artistID).click(function(){
       var song    = $(songID),
@@ -26,5 +38,33 @@ $(document).ready(function(){
         return song.attr("class") === "song"
       }
     });
+  }
+
+  function colorizeSong(songID){
+    var song = $(songID)
+
+    song.find("#colorize").click(function(){
+      song.toggleClass("colorize")
+    })
+  }
+
+  function increaseText(songID){
+    var song = $(songID)
+
+    song.find("#increase-text-size").click(function(){
+      var fontSize = parseInt(song.css("font-size").slice(0, -2))
+
+      song.css("font-size", fontSize + (fontSize * 0.1) + "px")
+    })
+  }
+
+  function decreaseText(songID){
+    var song = $(songID)
+
+    song.find("#decrease-text-size").click(function(){
+      var fontSize = parseInt(song.css("font-size").slice(0, -2))
+
+      song.css("font-size", fontSize - (fontSize * 0.1) + "px")
+    })
   }
 });
