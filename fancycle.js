12298eb0d17fa4fdcedc946ccf8e4bdedd6767a0
novohispano <novohispano@gmail.com>
2014-10-13 21:41:39 -0600

    Add first example

--- fancycle.js
+++ fancycle.js
@@ -1,1 +1,11 @@
-$(document).ready(function(){});
+$(document).ready(function(){
+  $("#justin").click(function(){
+    var counter = parseInt($("#baby").find("#view-counter").text())
+
+    $("#baby").toggleClass("hidden")
+
+    if ($("#baby").attr("class") === "song hidden") { counter++ }
+
+    $("#baby").find("#view-counter").text(counter)
+  });
+});
